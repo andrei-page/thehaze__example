2d5afa81194d0a3754679585b077dc2fea1fe240
andrei-page <andrei.web.io@gmail.com>
2018-08-12 02:14:19 +0300

    add a menu sticky and add sidebar

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,25 @@
-$(function() {
-  console.log('It works! ;)');
+$(" .b-menu__item-arrow").click(function(){
+    $("#menu-remove").addClass("remove-menu");
+    if($("#menu-remove").hasClass("remove-menu")){
+      $(" .b-menu__item-arrow").click(function(){
+        $("#menu-remove").addClass("active-menu");
+        $("#menu-remove").removeClass("remove-menu");
+      });
+    }
+     if($("#menu-remove").hasClass("active-menu")){
+      $(" .b-menu__item-arrow").click(function(){
+        $("#menu-remove").addClass("remove-menu");
+        $("#menu-remove").removeClass("active-menu");
+      });
+    }
+   
 });
+
+
+window.onscroll = function () {
+  if ($(document).scrollTop() >= 30) {
+      $(".b-header__box-top").addClass("b-header__box-small");
+  } else {
+      $(".b-header__box-top").removeClass("b-header__box-small");
+  }
+};
