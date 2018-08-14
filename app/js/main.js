e9a0b706a51b093375e3cb89623bf855ac0734ed
andrei-page <andrei.web.io@gmail.com>
2018-08-15 00:52:28 +0300

    add a service page

--- app/js/main.js
+++ app/js/main.js
@@ -55,3 +55,23 @@ $("#tab-2").click(function(){
   $("#tab-content1").css("display","none")
   $("#tab-content2").css("display","block")
 });
+
+(function($) {
+
+    $('.b-accordion a').click(function(j) {
+        var dropDown = $(this).closest('li').find('p');
+
+        $(this).closest('.b-accordion').find('p').not(dropDown).slideUp();
+
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+        } else {
+            $(this).closest('.b-accordion').find('a.active').removeClass('active');
+            $(this).addClass('active');
+        }
+
+        dropDown.stop(false, true).slideToggle();
+
+        j.preventDefault();
+    });
+})(jQuery);
